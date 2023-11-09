22a1f201044b21d8685cf19dc298c0c04c1811c1
ManasRanjanBariha <manasbariha779@gmail.com>
2023-11-09 21:33:41 +0530

    nov 7th

---
+++ java/DeletetheMiddleNodeofaLinkedList.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode slowest=head;
+        if(head==null || head.next==null)  
+        {
+            return null;
+        }
+
+        while(fast != null && fast.next!=null  )
+        {
+            fast=fast.next.next;
+            slowest=slow;
+            slow=slow.next;
+        }
+        slowest.next=slow.next;
+        return head;
+
+    }
+}
